65745cba476df62c4725c5e9843a14b07689b39e
benja2998 <benja2998@duck.com>
2025-07-09 01:11:06 -0400

    Development is back! (#41)
    Kali in Batch is now at 5.0, and better than ever.

--- CONTRIBUTING.md
+++ CONTRIBUTING.md
@@ -1,36 +1,47 @@
 Contributing to Kali in Batch
 =============================
+
 We welcome contributions from the community, but they must follow these guidelines:
+
 * Your code should be tested.
     To test the code, run this in a command prompt in the directory that contains the script:
+
     ```batch
     kali_in_batch.bat
     ```
+
     Use it like you would use a normal Kali Linux environment. If anything fails, do not submit your contribution until it is fixed.
 * Your code should follow the style of the rest of the code.
-    * Use `rem` for comments, not `::`.
-    * Use proper tabbing.
+  * Use `rem` for comments, not `::`.
+  * Use proper tabbing.
 
         For example, do this:
+
         ```batch
         if "%foo%"=="bar" (
             echo foo
         )
         ```
+
         And not this:
+
         ```batch
         if "%foo%"=="bar" (
         echo foo
         )
         ```
+
 * Your code should have comments documenting what it does.
 
     For example, do this:
+
     ```batch
     rem Set foo to bar
     set foo=bar
     ```
+
     And not this:
+
     ```batch
     set foo=bar
     ```
@@ -41,9 +52,11 @@ Where to start?
 Visit [this link](https://github.com/Kali-in-Batch/kali-in-batch/fork) to fork the Kali in Batch repository.
 
 Clone your fork to your local machine:
+
 ```bash
 git clone git@github.com:your-username/kali-in-batch.git
 ```
+
 From there, you can make changes to the code.
 
 *Note: You can also use https, and the name of the repo may be different if you specify a different name when you fork the repo.*
--- src/kali_in_batch.bat
+++ src/kali_in_batch.bat
@@ -4,32 +4,8 @@ setlocal enabledelayedexpansion
 
 rem kali_in_batch.bat
 rem    * Main script for the Kali in Batch project.
-rem    * Handles installation, boot process, and passes variables to the pwsh scripts used in the software.
-rem    * License:
-rem
-rem ======================================================================================
-rem MIT License
-rem
-rem Copyright (c) 2025 benja2998
-rem
-rem Permission is hereby granted, free of charge, to any person obtaining a copy
-rem of this software and associated documentation files (the "Software"), to deal
-rem in the Software without restriction, including without limitation the rights
-rem to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
-rem copies of the Software, and to permit persons to whom the Software is
-rem furnished to do so, subject to the following conditions:
-rem
-rem The above copyright notice and this permission notice shall be included in all
-rem copies or substantial portions of the Software.
-rem
-rem THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
-rem IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
-rem FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
-rem AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
-rem LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
-rem OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
-rem SOFTWARE.
-rem =======================================================================================
+rem    * Handles installation, boot process and sets up the bash environment.
+rem    * Licensed under the Apache-2.0.
 
 rem Color Definitions
 set "COLOR_RESET=[0m"
@@ -100,14 +76,12 @@ if not exist "%APPDATA%\kali_in_batch" (
         exit /b
     )
 
-    if not exist "%~dp0\bin\bash.exe" (
-        echo Bash not found. Please get the latest version of Kali in Batch from https://github.com/Kali-in-Batch/kali-in-batch/releases/latest/download/kali_in_batch.zip.
+    if not exist "%~dp0bin\bash.exe" (
+        echo Bash not found. Did you forget to run setup.bat?
         exit /b
     )
 
-    set "bash_path=%~dp0\bin\bash.exe"
-
-    rem The check above is so we can write some parts of the script in bash.
+    set "bash_path=%~dp0bin\bash.exe"
     
     cls
     echo !COLOR_INFO!Kali in Batch Installer!COLOR_RESET!
@@ -156,8 +130,8 @@ if not exist "%APPDATA%\kali_in_batch" (
         mkdir "!kaliroot!\tmp"
         mkdir "!kaliroot!\usr"
         mkdir "!kaliroot!\usr\bin"
-        rem Copy contents of %~dp0\bin to !kaliroot!\usr\bin.
-        copy /y "%~dp0\bin\*" "!kaliroot!\usr\bin"
+        rem Copy contents of %~dp0bin to !kaliroot!\usr\bin.
+        copy /y "%~dp0bin\*" "!kaliroot!\usr\bin"
         set "bash_path=!kaliroot!\usr\bin\bash.exe"
         echo Checking dependencies...
     )
@@ -170,14 +144,6 @@ if not exist "%APPDATA%\kali_in_batch" (
     if !errorlevel! neq 0 (
         echo Installing Neovim from winget...
         winget install --id Neovim.Neovim -e --source winget
-    )
-
-    where pwsh >nul 2>&1
-    if !errorlevel! neq 0 (
-        echo Installing PowerShell from winget...
-        winget install --id Microsoft.PowerShell -e --source winget
-        pause >nul
-        exit
     )
     mkdir "%APPDATA%\kali_in_batch" >nul 2>&1
     @echo on
@@ -201,69 +167,71 @@ if !command!==help (
     echo help - Displays this message.
     echo done - Finishes setup and exits the preinstall shell.
     echo wipe - Wipes the Kali in Batch root filesystem.
-    echo add-kalirc - Adds a .kalirc file to the home directory.
+    echo add-bashrc - Adds a .bashrc file to the home directory.
     echo clear - Clears the screen.
-    echo add-kibprompt - Adds a .kibprompt file to the home directory.
-    echo edit-kalirc - Edits the .kalirc file.
-    echo edit-kibprompt - Edits the .kibprompt file.
+    echo edit-bashrc - Edits the .bashrc file.
     goto preinstall
 ) else if !command!==done (
     echo Finishing setup...
+    if not exist "!kaliroot!\home\!username!\.bashrc" (
+        echo Please run add-bashrc before finishing setup
+        goto preinstall
+    )
     cls
     goto boot
 ) else if !command!==wipe (
     goto wipe
-) else if !command!==add-kalirc (
-    echo Adding .kalirc file...
-    echo echo "██   ██  █████  ██      ██     ██ ███    ██     ██████   █████  ████████  ██████ ██   ██" >> "!kaliroot!\home\!username!\.kalirc"
-    echo echo "██  ██  ██   ██ ██      ██     ██ ████   ██     ██   ██ ██   ██    ██    ██      ██   ██" >> "!kaliroot!\home\!username!\.kalirc"
-    echo echo "█████   ███████ ██      ██     ██ ██ ██  ██     ██████  ███████    ██    ██      ███████" >> "!kaliroot!\home\!username!\.kalirc"
-    echo echo "██  ██  ██   ██ ██      ██     ██ ██  ██ ██     ██   ██ ██   ██    ██    ██      ██   ██" >> "!kaliroot!\home\!username!\.kalirc"
-    echo echo "██   ██ ██   ██ ███████ ██     ██ ██   ████     ██████  ██   ██    ██     ██████ ██   ██" >> "!kaliroot!\home\!username!\.kalirc"
-    echo echo "" >> "!kaliroot!\home\!username!\.kalirc"
+) else if !command!==add-bashrc (
+    echo Adding .bashrc file...
+
+    (
+        echo # THINGS WILL BREAK IF YOU MODIFY ANYTHING BELOW
+        echo export BASH_ENV="$HOME/.bashrc"
+        echo export PATH="/usr/bin:/bin:/cygdrive/c/Windows:/cygdrive/c/Windows/System32:/cygdrive/c/Program Files (x86)/Nmap:/cygdrive/c/Users/!username!/AppData/Local/Microsoft/WindowsApps:/cygdrive/c/Program Files/WindowsApps:/cygdrive/c/Program Files/Neovim/bin"
+        echo PS1=$'\[\e[34m\]┌──^(\[\e[0;38;5;45m\]\u\[\e[34m\]㉿\[\e[0;38;5;203m\]\h\[\e[34m\]^)-[\[\e[32m\]\w\[\e[34m\]]\n└─\[\e[0m\]\$ '
+        echo alias touch='touch.bat'
+        echo alias uname='uname.bat'
+        echo alias whoami='whoami.bat'
+        echo alias pkg='pkg.bat'
+        echo alias echo='echo.bat'
+        echo alias cat='cat.bat'
+        echo alias ls='ls.bat'
+        echo alias nc='busybox nc'
+        echo alias netcat='busybox nc'
+        echo   mapfile -t __busybox_applets ^< ^<^(busybox --list^)
+        echo   command_not_found_handle^(^) {
+        echo     for applet in "${__busybox_applets[@]}"; do
+        echo       if [[ "$1" == "$applet" ]]; then
+        echo         busybox "$@"
+        echo         return $?
+        echo       fi
+        echo     done
+        echo     printf '^%%s: command not found\n' "$1" ^>^&2
+        echo     return 127
+        echo   }
+        echo # Here, it is safe to add more commands below
+        echo.
+    ) >> "!kaliroot!\home\!username!\.bashrc"
+
     echo Done.
     goto preinstall
 ) else if !command!==clear (
     cls
     goto preinstall
-) else if !command!==add-kibprompt (
-    echo Adding .kibprompt file...
-    echo Write-Host "$env:USERNAME@$env:COMPUTERNAME" -ForegroundColor $colorRed -NoNewLine >> "!kaliroot!\home\!username!\.kibprompt"
-    echo Write-Host ":" -NoNewLine >> "!kaliroot!\home\!username!\.kibprompt"
-    echo Write-Host "$kaliPathtwo" -ForegroundColor $colorBlue -NoNewLine  >> "!kaliroot!\home\!username!\.kibprompt"
-    echo Write-Host "$ " -NoNewLine >> "!kaliroot!\home\!username!\.kibprompt"
-    echo Done.
-    goto preinstall
-) else if !command!==edit-kalirc (
-    rem Make sure the .kalirc file exists
-    if exist "!kaliroot!\home\!username!\.kalirc" (
-        rem Open the .kalirc file in Nvim or Notepad
+) else if !command!==edit-bashrc (
+    rem Make sure the .bashrc file exists
+    if exist "!kaliroot!\home\!username!\.bashrc" (
+        rem Open the .bashrc file in Nvim or Notepad
         nvim --version >nul 2>&1
         if !errorlevel!==0 (
-            nvim "!kaliroot!\home\!username!\.kalirc"
+            nvim "!kaliroot!\home\!username!\.bashrc"
         ) else (
-            notepad "!kaliroot!\home\!username!\.kalirc"
+            notepad "!kaliroot!\home\!username!\.bashrc"
         )
         goto preinstall
     ) else (
-        rem The .kalirc file doesn't exist!
-        echo Please run the 'add-kalirc' command first.
-        goto preinstall
-    )
-) else if !command!==edit-kibprompt (
-    rem Make sure the .kibprompt file exists
-    if exist "!kaliroot!\home\!username!\.kibprompt" (
-        rem Open the .kibprompt file in Nvim or Notepad
-        nvim --version >nul 2>&1
-        if !errorlevel!==0 (
-            nvim "!kaliroot!\home\!username!\.kibprompt"
-        ) else (
-            notepad "!kaliroot!\home\!username!\.kibprompt"
-        )
-        goto preinstall
-    ) else (
-        rem The .kibprompt file doesn't exist!
-        echo Please run the 'add-kibprompt' command first.
+        rem The .bashrc file doesn't exist!
+        echo Please run the 'add-bashrc' command first.
         goto preinstall
     )
 ) else (
@@ -305,25 +273,15 @@ if exist !kaliroot! (
     subst !kaliroot! "C:\Users\!username!\kali" >nul 2>&1
 )
 
-rem Check if %APPDATA%\kali_in_batch\powershell exists and delete it if it does
-if exist "%APPDATA%\kali_in_batch\powershell" (
-    rmdir /s /q "%APPDATA%\kali_in_batch\powershell"
-)
-rem So if the user runs git pull on the local repository to get updates, they can get the latest version of the pwsh scripts.
-mkdir "%APPDATA%\kali_in_batch\powershell"
-cd /d "%~dp0" & rem Needed incase the user is using a Windows Terminal profile or something that changes the current directory
-rem Copy .\powershell\* to %APPDATA%\kali_in_batch\powershell
-xcopy "%~dp0\powershell\*" "%APPDATA%\kali_in_batch\powershell" /s /y >nul
-
-rem Copy %~dp0\bin\* to !kaliroot!\usr\bin
-xcopy "%~dp0\bin\*" "!kaliroot!\usr\bin" /s /y >nul
+rem Copy %~dp0bin\* to !kaliroot!\usr\bin
+xcopy "%~dp0bin\*" "!kaliroot!\usr\bin\" /s /y >nul
 
 rem Check if VERSION.txt exists and delete it if it does
 if exist "%APPDATA%\kali_in_batch\VERSION.txt" (
     del "%APPDATA%\kali_in_batch\VERSION.txt"
 )
 rem Create VERSION.txt
-echo 4.2.0>"%APPDATA%\kali_in_batch\VERSION.txt"
+echo 5.0>"%APPDATA%\kali_in_batch\VERSION.txt"
 
 echo Starting services...
 where nmap >nul 2>&1
@@ -382,101 +340,22 @@ goto startup
 
 :startup
 rem Navigate to home directory
-cd /d "!kaliroot!\home\!username!"
+cd /d "C:\Users\!username!\kali\home\!username!"
 if %errorlevel% neq 0 (
     echo error
     pause >nul
     exit
 )
-set "kalirc=!cd!\.kalirc"
+set "bashrc=!cd!\.bashrc"
 set "home_dir=!cd!"
-if exist !kalirc! (
-    set bash_current_dir=!cd! >nul 2>&1
-    set bash_current_dir=!cd:\=/! >nul 2>&1
-    set bash_current_dir=!bash_current_dir:C:=! >nul 2>&1
-    set bash_current_dir=!bash_current_dir:D:=! >nul 2>&1
-    set bash_current_dir=!bash_current_dir:E:=! >nul 2>&1
-    set bash_current_dir=!bash_current_dir:F:=! >nul 2>&1
-    set bash_current_dir=!bash_current_dir:G:=! >nul 2>&1
-    set bash_current_dir=!bash_current_dir:H:=! >nul 2>&1
-    set bash_current_dir=!bash_current_dir:I:=! >nul 2>&1
-    set bash_current_dir=!bash_current_dir:J:=! >nul 2>&1
-    set bash_current_dir=!bash_current_dir:K:=! >nul 2>&1
-    set bash_current_dir=!bash_current_dir:L:=! >nul 2>&1
-    set bash_current_dir=!bash_current_dir:M:=! >nul 2>&1
-    set bash_current_dir=!bash_current_dir:N:=! >nul 2>&1
-    set bash_current_dir=!bash_current_dir:O:=! >nul 2>&1
-    set bash_current_dir=!bash_current_dir:P:=! >nul 2>&1
-    set bash_current_dir=!bash_current_dir:Q:=! >nul 2>&1
-    set bash_current_dir=!bash_current_dir:R:=! >nul 2>&1
-    set bash_current_dir=!bash_current_dir:S:=! >nul 2>&1
-    set bash_current_dir=!bash_current_dir:T:=! >nul 2>&1
-    set bash_current_dir=!bash_current_dir:U:=! >nul 2>&1
-    set bash_current_dir=!bash_current_dir:V:=! >nul 2>&1
-    set bash_current_dir=!bash_current_dir:W:=! >nul 2>&1
-    set bash_current_dir=!bash_current_dir:X:=! >nul 2>&1
-    set bash_current_dir=!bash_current_dir:Y:=! >nul 2>&1
-    set bash_current_dir=!bash_current_dir:Z:=! >nul 2>&1
-    "!bash_path!" -c "cd !bash_current_dir!; source .kalirc" 2>&1
-    goto shell
-) else (
-    echo No .kalirc file found.
-    goto shell
-)
 
 :shell
-set current_dir=!cd!
-if !current_dir!==!home_dir! (
-    set current_dir=~
-) else (
-    set current_dir=!cd!
-)
 
-rem Replace backslashes with forward slashes in !current_dir!
-set current_dir=!current_dir:\=/!
-rem Replace drive letters with nothing in !current_dir!
-set current_dir=!current_dir:C:=!
-set current_dir=!current_dir:D:=!
-set current_dir=!current_dir:E:=!
-set current_dir=!current_dir:F:=!
-set current_dir=!current_dir:G:=!
-set current_dir=!current_dir:H:=!
-set current_dir=!current_dir:I:=!
-set current_dir=!current_dir:J:=!
-set current_dir=!current_dir:K:=!
-set current_dir=!current_dir:L:=!
-set current_dir=!current_dir:M:=!
-set current_dir=!current_dir:N:=!
-set current_dir=!current_dir:O:=!
-set current_dir=!current_dir:P:=!
-set current_dir=!current_dir:Q:=!
-set current_dir=!current_dir:R:=!
-set current_dir=!current_dir:S:=!
-set current_dir=!current_dir:T:=!
-set current_dir=!current_dir:U:=!
-set current_dir=!current_dir:V:=!
-set current_dir=!current_dir:W:=!
-set current_dir=!current_dir:X:=!
-set current_dir=!current_dir:Y:=!
-set current_dir=!current_dir:Z:=!
+echo ██   ██  █████  ██      ██     ██ ███    ██     ██████   █████  ████████  ██████ ██   ██
+echo ██  ██  ██   ██ ██      ██     ██ ████   ██     ██   ██ ██   ██    ██    ██      ██   ██
+echo █████   ███████ ██      ██     ██ ██ ██  ██     ██████  ███████    ██    ██      ███████
+echo ██  ██  ██   ██ ██      ██     ██ ██  ██ ██     ██   ██ ██   ██    ██    ██      ██   ██
+echo ██   ██ ██   ██ ███████ ██     ██ ██   ████     ██████  ██   ██    ██     ██████ ██   ██
+echo.
 
-goto new_shell_prompt
-
-:new_shell_prompt
-
-rem Check if PS 7+ is installed
-rem PS 7+ uses pwsh.exe instead of pwsh.exe
-where pwsh.exe >nul 2>&1
-if !errorlevel!==0 (
-    rem It is installed!
-) else (
-    rem It is not installed!
-    echo !COLOR_ERROR!pwsh is not installed.!COLOR_RESET!
-    echo !COLOR_INFO!Installing pwsh...!COLOR_RESET!
-    winget install --id Microsoft.PowerShell -e --source winget
-    echo Press any key to exit...
-    pause >nul
-    exit /b
-)
-
-pwsh.exe -noprofile -executionpolicy bypass -file "%APPDATA%\kali_in_batch\powershell\shell_prompt.ps1" -bashexepath "!bash_path!" -kaliroot "!kaliroot!" -utils_path "%~dp0bin"
+!bash_path!
