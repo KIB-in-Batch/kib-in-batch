d355cd06ec2e01fd74aea01d22d75a6faab53fce
benja2998 <benja2998@duck.com>
2025-07-10 00:57:35 -0400

    Fix winget not being in PATH (#46)
    add winget

--- src/bin/uname.bat
+++ src/bin/uname.bat
@@ -40,7 +40,7 @@ goto :eof
 
 :release
 
-echo 6.0
+echo 6.1
 goto :eof
 
 :machine
@@ -60,7 +60,7 @@ goto :eof
 
 :version
 
-echo Uname for Kali in Batch 6.0
+echo Uname for Kali in Batch 6.1
 echo This is Apache-2.0 licensed free software. There is NO WARRANTY.
 goto :eof
 
