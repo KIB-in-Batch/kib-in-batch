8a324e683cf49cc5f6a4fc092d4df4f1ce6204e8
benja2998 <197428407+benja2998@users.noreply.github.com>
2025-07-12 17:56:04 -0400

    Fix the worst bug EVER

--- src/bin/uname.bat
+++ src/bin/uname.bat
@@ -40,7 +40,7 @@ goto :eof
 
 :release
 
-echo 6.3
+echo 6.4
 goto :eof
 
 :machine
@@ -60,7 +60,7 @@ goto :eof
 
 :version
 
-echo Uname for Kali in Batch 6.3
+echo Uname for Kali in Batch 6.4
 echo This is Apache-2.0 licensed free software. There is NO WARRANTY.
 goto :eof
 
