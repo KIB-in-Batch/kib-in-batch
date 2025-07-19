868aef0075c2c6395424a7e36aa8449b384b4cf8
benja2998 <benja2998@duck.com>
2025-07-19 17:14:59 -0400

    8.0-alpha1

--- src/kali_in_batch.bat
+++ src/kali_in_batch.bat
@@ -5,7 +5,22 @@ setlocal enabledelayedexpansion
 rem kali_in_batch.bat
 rem    * Main script for the Kali in Batch project.
 rem    * Handles installation, boot process and sets up the bash environment.
-rem    * Licensed under the Apache-2.0.
+rem    * Licensed under the GPL-2.0-only.
+rem Copyright (C) 2025 benja2998
+rem
+rem This program is free software; you can redistribute it and/or modify
+rem it under the terms of the GNU General Public License as published by
+rem the Free Software Foundation; ONLY version 2 of the License.
+rem
+rem This program is distributed in the hope that it will be useful,
+rem but WITHOUT ANY WARRANTY; without even the implied warranty of
+rem MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+rem GNU General Public License for more details.
+rem
+rem You should have received a copy of the GNU General Public License
+rem along with this program; if not, write to the Free Software
+rem Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
+
 
 rem Color Definitions
 set "COLOR_RESET=[0m"
@@ -76,12 +91,13 @@ if not exist "%APPDATA%\kali_in_batch" (
         exit /b
     )
 
-    if not exist "%~dp0bin\bash.exe" (
+    if not exist "%~dp0bin\busybox.exe" (
         echo Bash not found. Did you forget to run setup.bat?
+        pause >nul
         exit /b
     )
 
-    set "bash_path=%~dp0bin\bash.exe"
+    set "busybox_path=%~dp0bin\busybox.exe"
     
     cls
     echo !COLOR_INFO!Kali in Batch Installer!COLOR_RESET!
@@ -108,8 +124,12 @@ if not exist "%APPDATA%\kali_in_batch" (
             mkdir "C:\Users\!username!\kali" >nul 2>&1
         )
         rem Ask for what drive letter to assign to the root filesystem.
-        set /p "driveletter=Enter the drive letter to assign to the root filesystem (e.g. Z:) >> "
-        echo Drive letter: !driveletter!
+        if "%~1"=="automated" (
+            set "driveletter=Z:"
+        ) else (
+            set /p "driveletter=Enter the drive letter to assign to the root filesystem (e.g. Z:) >> "
+            echo Drive letter: !driveletter!
+        )
         rem Make sure it isn't an existing drive letter.
         if exist !driveletter! (
             echo Drive letter already in use.
@@ -130,11 +150,22 @@ if not exist "%APPDATA%\kali_in_batch" (
         mkdir "!kaliroot!\tmp"
         mkdir "!kaliroot!\usr"
         mkdir "!kaliroot!\etc"
-        copy /y "%~dp0os-release" "!kaliroot!\etc\os-release" >nul 2>&1
         mkdir "!kaliroot!\usr\bin"
-        rem Copy contents of %~dp0bin to !kaliroot!\usr\bin.
-        copy /y "%~dp0bin\*" "!kaliroot!\usr\bin"
-        set "bash_path=!kaliroot!\usr\bin\bash.exe"
+        mkdir "!kaliroot!\usr\lib"
+        mkdir "!kaliroot!\usr\share"
+        mkdir "!kaliroot!\usr\local"
+        mkdir "!kaliroot!\usr\libexec"
+        mkdir "!kaliroot!\var"
+        mkdir "!kaliroot!\root"
+        xcopy "%~dp0bin\*" "!kaliroot!\usr\bin\" /s /y >nul
+        xcopy "%~dp0etc\*" "!kaliroot!\etc\" /s /y >nul
+        xcopy "%~dp0lib\*" "!kaliroot!\usr\lib\" /s /y >nul
+        xcopy "%~dp0share\*" "!kaliroot!\usr\share\" /s /y >nul
+        xcopy "%~dp0libexec\*" "!kaliroot!\usr\libexec\" /s /y >nul
+        copy /y "%~dp0kibenv" "!kaliroot!\etc\.kibenv" >nul
+        copy /y "%~dp0os-release" "!kaliroot!\etc\os-release" >nul
+        echo # Add commands to run on startup here>"!kaliroot!\home\!username!\.bashrc"
+        echo # Add commands to run on startup here>"!kaliroot!\root\.bashrc"
         echo Checking dependencies...
     )
     where nmap >nul 2>&1
@@ -153,78 +184,14 @@ if not exist "%APPDATA%\kali_in_batch" (
     @echo off
     rem Set install part to the txt file created in installer
     set /p kaliroot=<"%APPDATA%\kali_in_batch\kaliroot.txt"
-    rem Enter the preinstall shell
-    echo Welcome to the preinstall shell. Please type 'help' for the commands you'll need to finish setup. Type 'done' to boot into Kali in Batch.
-    goto preinstall
 )
 rem Set install part to the txt file created in installer
 set /p kaliroot=<"%APPDATA%\kali_in_batch\kaliroot.txt"
+mkdir "%APPDATA%\kali_in_batch\emptydir" >nul 2>&1
+echo Hello world!>"%APPDATA%\kali_in_batch\emptydir\hello.txt"
 cls
+copy "%~dp0kibenv" "!kaliroot!\etc\.kibenv" /y >nul 2>&1
 goto boot
-
-:preinstall
-set /p "command=!COLOR_PROMPT!!COLOR_UNDERLINE!kali-in-batch-preinstall!COLOR_RESET! >> "
-if !command!==help (
-    echo Commands:
-    echo help - Displays this message.
-    echo done - Finishes setup and exits the preinstall shell.
-    echo wipe - Wipes the Kali in Batch root filesystem.
-    echo add-kibenv - Adds a .kibenv file to the file system.
-    echo add-bashrc - Adds a .bashrc file to the home directory.
-    echo edit-bashrc - Edits the .bashrc file.
-    echo clear - Clears the screen.
-    goto preinstall
-) else if !command!==done (
-    echo Finishing setup...
-    if not exist "!kaliroot!\etc\.kibenv" (
-        echo Please run add-kibenv before finishing setup
-        goto preinstall
-    )
-    cls
-    goto boot
-) else if !command!==wipe (
-    goto wipe
-) else if !command!==add-kibenv (
-    echo Adding .kibenv file...
-
-    copy "%~dp0kibenv" "!kaliroot!\etc\.kibenv" /y >nul
-
-    echo Done.
-    goto preinstall
-) else if !command!==add-bashrc (
-    echo Adding .bashrc file...
-
-    echo # Add commands to run on startup here>"!kaliroot!\home\!username!\.bashrc"
-
-    echo Done.
-    goto preinstall
-) else if !command!==clear (
-    cls
-    goto preinstall
-) else if !command!==edit-bashrc (
-    rem Make sure the .bashrc file exists
-    if exist "!kaliroot!\home\!username!\.bashrc" (
-        rem Open the .bashrc file in Nvim or Notepad
-        nvim --version >nul 2>&1
-        if !errorlevel!==0 (
-            nvim "!kaliroot!\home\!username!\.bashrc"
-        ) else (
-            notepad "!kaliroot!\home\!username!\.bashrc"
-        )
-        goto preinstall
-    ) else (
-        rem The .bashrc file doesn't exist!
-        echo Please run the 'add-bashrc' command first.
-        goto preinstall
-    )
-) else (
-    if "!command!"=="" (
-        rem Silently ignore empty commands
-        goto preinstall
-    )
-    echo Invalid command.
-    goto preinstall
-)
 
 :wipe
 echo Wiping kali rootfs...
@@ -244,6 +211,16 @@ exit
 rem Boot process for Kali in Batch
 rem It handles essential checks to make sure Kali in Batch can boot properly.
 
+for /f "delims=" %%i in ('powershell -command "[System.Environment]::OSVersion.Version.ToString()"') do set kernelversion=%%i
+
+cls
+
+echo Welcome to Kali in Batch 8.0 ^(%PROCESSOR_ARCHITECTURE%^)
+echo Booting system...
+echo ------------------------------------------------
+::                                                                 |
+<nul set /p "=Assigning drive letter...                            "
+
 rem Check if the !kaliroot! virtual drive letter is still assigned
 if exist !kaliroot! (
     rem Nothing to do
@@ -252,71 +229,187 @@ if exist !kaliroot! (
     subst !kaliroot! "C:\Users\!username!\kali" >nul 2>&1
 )
 
-rem Copy %~dp0bin\* to !kaliroot!\usr\bin
-xcopy "%~dp0bin\*" "!kaliroot!\usr\bin\" /s /y >nul
+<nul set /p "=[ !COLOR_SUCCESS!OK!COLOR_RESET! ]"
+echo.
 
-rem Copy kibenv
-copy /y "%~dp0kibenv" "!kaliroot!\etc\.kibenv" >nul
+::                                                                 |
+<nul set /p "=Checking if rescue is required...                    "
 
-rem Copy os-release
-copy /y "%~dp0os-release" "!kaliroot!\etc\os-release" >nul
+set "rescue_required=0"
+
+if not exist "!kaliroot!\etc" set rescue_required=1
+if not exist "!kaliroot!\tmp" set rescue_required=1
+if not exist "!kaliroot!\usr" set rescue_required=1
+if not exist "!kaliroot!\bin" set rescue_required=1
+if not exist "!kaliroot!\usr\bin" set rescue_required=1
+if not exist "!kaliroot!\usr\local" set rescue_required=1
+if not exist "!kaliroot!\usr\share" set rescue_required=1
+if not exist "!kaliroot!\usr\lib" set rescue_required=1
+if not exist "!kaliroot!\home" set rescue_required=1
+if not exist "!kaliroot!\home\!username!" set rescue_required=1
+if not exist "!kaliroot!\usr\libexec" set rescue_required=1
+if not exist "!kaliroot!\var" set rescue_required=1
+if not exist "!kaliroot!\root" set rescue_required=1
+
+if %rescue_required%==1 (
+    if not exist "!kaliroot!\etc" mkdir "!kaliroot!\etc"
+    if not exist "!kaliroot!\tmp" mkdir "!kaliroot!\tmp"
+    if not exist "!kaliroot!\usr" (
+        mkdir "!kaliroot!\usr"
+        mkdir "!kaliroot!\usr\bin"
+        mkdir "!kaliroot!\usr\share"
+        mkdir "!kaliroot!\usr\lib"
+        mkdir "!kaliroot!\usr\local"
+        mkdir "!kaliroot!\usr\libexec"
+    )
+    if not exist "!kaliroot!\usr\bin" mkdir "!kaliroot!\usr\bin"
+    if not exist "!kaliroot!\usr\share" mkdir "!kaliroot!\usr\share"
+    if not exist "!kaliroot!\usr\lib" mkdir "!kaliroot!\usr\lib"
+    if not exist "!kaliroot!\usr\local" mkdir "!kaliroot!\usr\local"
+    if not exist "!kaliroot!\usr\libexec" mkdir "!kaliroot!\usr\libexec
+    if not exist "!kaliroot!\bin" mkdir "!kaliroot!\bin"
+    if not exist "!kaliroot!\home" mkdir "!kaliroot!\home"
+    if not exist "!kaliroot!\home\!username!" mkdir "!kaliroot!\home\!username!"
+    if not exist "!kaliroot!\var" mkdir "!kaliroot!\var"
+    if not exist "!kaliroot!\root" mkdir "!kaliroot!\root"
+)
+
+<nul set /p "=[ !COLOR_SUCCESS!OK!COLOR_RESET! ]"
+echo.
+
+::                                                                 |
+<nul set /p "=Initializing ROOT environment variable...            "
+
+rem Initialize ROOT variable
+set "ROOT=0"
+
+<nul set /p "=[ !COLOR_SUCCESS!OK!COLOR_RESET! ]"
+echo.
+
+::                                                                 |
+<nul set /p "=Updating core files...                               "
+
+xcopy "%~dp0bin\*" "!kaliroot!\usr\bin\" /s /y >nul 2>&1
+xcopy "%~dp0etc\*" "!kaliroot!\etc\" /s /y >nul 2>&1
+xcopy "%~dp0lib\*" "!kaliroot!\usr\lib\" /s /y >nul 2>&1
+xcopy "%~dp0share\*" "!kaliroot!\usr\share\" /s /y >nul 2>&1
+xcopy "%~dp0libexec\*" "!kaliroot!\usr\libexec\" /s /y >nul 2>&1
+copy /y "%~dp0kibenv" "!kaliroot!\etc\.kibenv" >nul 2>&1
+copy /y "%~dp0os-release" "!kaliroot!\etc\os-release" >nul 2>&1
+
+if !errorlevel! neq 0 (
+    <nul set /p "=[ !COLOR_ERROR!FAILED!COLOR_RESET! ]"
+    pause >nul
+    exit /b 1
+) else (
+    <nul set /p "=[ !COLOR_SUCCESS!OK!COLOR_RESET! ]"
+    echo.
+)
 
 rem Check if VERSION.txt exists and delete it if it does
 if exist "%APPDATA%\kali_in_batch\VERSION.txt" (
     del "%APPDATA%\kali_in_batch\VERSION.txt"
 )
 rem Create VERSION.txt
-echo 7.1>"%APPDATA%\kali_in_batch\VERSION.txt"
+echo 8.0>"%APPDATA%\kali_in_batch\VERSION.txt"
 
-echo Starting services...
+::                                                                 |
+<nul set /p "=Starting Nmap service...                             "
+
 where nmap >nul 2>&1
 if !errorlevel! neq 0 (
-    echo !COLOR_ERROR!Error: Failed to start Nmap service: Nmap not found.!COLOR_RESET!
-    echo Please install Nmap from https://nmap.org/download.html
-    pause
-    exit
+    <nul set /p "=[ !COLOR_ERROR!FAILED!COLOR_RESET! ]"
+    pause >nul
+    exit /b 1
+) else (
+    <nul set /p "=[ !COLOR_SUCCESS!OK!COLOR_RESET! ]"
+    echo.
 )
+
+::                                                                 |
+<nul set /p "=Starting Neovim service...                           "
+
 where nvim >nul 2>&1
 if !errorlevel! neq 0 (
-    echo !COLOR_ERROR!Error: Failed to start Neovim service: Neovim not found.!COLOR_RESET!
+    <nul set /p "=[ !COLOR_ERROR!FAILED!COLOR_RESET! ]"
+) else (
+    <nul set /p "=[ !COLOR_SUCCESS!OK!COLOR_RESET! ]"
+    echo.
 )
 
-if not exist "!kaliroot!\usr\bin\bash.exe" (
-    echo !COLOR_ERROR!Error: Failed to start Bash service: Bash not found.!COLOR_RESET!
-    pause >nul
-    exit
-)
+::                                                                 |
+<nul set /p "=Starting Bash service...                             "
 
 if not exist "!kaliroot!\usr\bin\busybox.exe" (
-    echo !COLOR_ERROR!Error: Failed to start Busybox service: Busybox not found.!COLOR_RESET!
+    <nul set /p "=[ !COLOR_ERROR!FAILED!COLOR_RESET! ]"
     pause >nul
-    exit
+    exit /b 1
+) else (
+    <nul set /p "=[ !COLOR_SUCCESS!OK!COLOR_RESET! ]"
+    echo.
 )
 
-set "bash_path=!kaliroot!\usr\bin\bash.exe"
+set "busybox_path=!kaliroot!\usr\bin\busybox.exe"
 
-where pwsh >nul 2>&1
-if !errorlevel! neq 0 (
-    echo !COLOR_ERROR!Error: Failed to start PowerShell service: PowerShell not found.!COLOR_RESET!
-    pause >nul
-    exit
-)
-echo Checking for updates...
-curl -# https://raw.githubusercontent.com/Kali-in-Batch/kali-in-batch/refs/heads/master/VERSION.txt >"!kaliroot!\tmp\VERSION.txt"
+::                                                                 |
+<nul set /p "=Checking for updates...                              "
+
+curl -s https://raw.githubusercontent.com/Kali-in-Batch/kali-in-batch/refs/heads/master/VERSION.txt >"!kaliroot!\tmp\VERSION.txt"
 rem Check if the version is the same
 set /p remote_version=<"!kaliroot!\tmp\VERSION.txt"
 set /p local_version=<"%APPDATA%\kali_in_batch\VERSION.txt"
 if !remote_version! neq !local_version! (
     rem Outdated Kali in Batch installation
-    echo !COLOR_WARNING!New version available!!COLOR_RESET!
-    echo !COLOR_WARNING!Remote version: !remote_version!!COLOR_RESET!
-    echo !COLOR_WARNING!Local version: !local_version!!COLOR_RESET!
+    <nul set /p "=[ !COLOR_WARNING!OUTDATED!COLOR_RESET! ]"
+    echo.
 ) else (
-    echo !COLOR_SUCCESS!You are running the latest version.!COLOR_RESET!
-    echo !COLOR_SUCCESS!Remote version: !remote_version!!COLOR_RESET!
-    echo !COLOR_SUCCESS!Local version: !local_version!!COLOR_RESET!
+    <nul set /p "=[ !COLOR_SUCCESS!OK!COLOR_RESET! ]"
+    echo.
 )
-timeout /t 1 >nul
+
+echo ------------------------------------------------
+
+echo !COLOR_SUCCESS!System boot completed.!COLOR_RESET!
+if "%~1"=="automated" (
+    del "!kaliroot!\tmp\VERSION.txt"
+    echo.
+    cls
+    goto startup
+) else (
+    echo Press any key to log into the system...
+    pause >nul
+    goto login
+)
+
+:login
+
+cls
+echo Kali in Batch 8.0
+echo Kernel !kernelversion! on an %PROCESSOR_ARCHITECTURE%
+echo.
+echo Users on this system: !username!, root
+echo.
+set /p loginkibusername=%COMPUTERNAME% login: 
+if "!loginkibusername!"=="!username!" (
+    rem Correct
+    set "USER=!username!"
+    echo !COLOR_SUCCESS!User found!COLOR_RESET!
+    echo Connecting to Bash service...
+    timeout /t 1 /nobreak >nul 2>&1
+) else if "!loginkibusername!"=="root" (
+    set "ROOT=1"
+    set "USER=root"
+    echo !COLOR_SUCCESS!User found!COLOR_RESET!
+    echo Connecting to Bash service...
+    timeout /t 1 /nobreak >nul 2>&1
+) else (
+    rem Incorrect
+    echo !COLOR_ERROR!User not found!COLOR_RESET!
+    echo Press any key to try again...
+    pause >nul
+    goto login
+)
+
 del "!kaliroot!\tmp\VERSION.txt"
 echo.
 cls
@@ -326,9 +419,9 @@ goto startup
 rem Navigate to home directory
 cd /d "C:\Users\!username!\kali\home\!username!"
 if %errorlevel% neq 0 (
-    echo Fatal error, please reinstall Kali in Batch
+    echo !COLOR_ERROR!Fatal error, unable to navigate to home directory.!COLOR_RESET!
     pause >nul
-    exit
+    exit /b 69
 )
 set "kibenv=!kaliroot!\etc\.kibenv"
 set "home_dir=!cd!"
@@ -342,4 +435,17 @@ echo ██  ██  ██   ██ ██      ██     ██ ██  █�
 echo ██   ██ ██   ██ ███████ ██     ██ ██   ████     ██████  ██   ██    ██     ██████ ██   ██
 echo.
 
-!bash_path! --rcfile "!kibenv!"
+if not exist "!kaliroot!\tmp" (
+    mkdir "!kaliroot!\tmp"
+)
+
+if "!ROOT!"=="0" (
+    set "HOME=!kaliroot!\home\!username!"
+) else (
+    set "HOME=!kaliroot!\root"
+)
+copy /y "%~dp0kibenv" "!HOME!\.profile" >nul 2>&1
+if not exist "!HOME!\.bashrc" (
+    echo. >"!HOME!\.bashrc"
+)
+"!busybox_path!" bash -l
