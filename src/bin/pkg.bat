3b63fce5fb8d0590d3fc7d54366d76757c11c2e5
benja2998 <benja2998@duck.com>
2025-07-20 23:56:06 -0400

    Remove setup.bat and make main script download busybox by itself, embed multiple files in main script, add useful guides (#69)

--- src/bin/pkg.bat
+++ src/bin/pkg.bat
@@ -21,22 +21,15 @@ rem You should have received a copy of the GNU General Public License
 rem along with this program; if not, write to the Free Software
 rem Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
 
-
-echo Kali in Batch is having a very much BREAKING update.
-echo This package manager will be available once new repositories for packages are set up.
-echo.
-echo Please be patient.
-echo If you prefer to speed up the process, you can contribute to the project by creating a pull request at
-echo https://github.com/Kali-in-Batch/kali-in-batch/pulls
-exit /b 69
-
 goto start
 
 :start
 
 rem Check if ROOT is set to 0
 if "%ROOT%" == "0" (
-    echo Please log in as the root user to run pkg
+    echo Please log in as the root user to run pkg, or you can:
+    echo 1. Run pkg with sudo ^(e.g. sudo pkg %*^)
+    echo 2. Use the su command to become root, then run unsu after you don't need root anymore.
     exit /b 1
 )
 
@@ -49,18 +42,45 @@ if "%*"=="" (
 
 :noargs
 
-echo Usage: pkg ^(install/remove/upgrade/search/list^)
+echo Usage: pkg ^(install/remove/upgrade/search/list/help^)
+exit
+
+:help
+
+echo Usage: pkg ^(install/remove/upgrade/search/list/help^)
+echo.
+echo Commands:
+echo   install  - Install a package by name.
+echo   remove   - Remove an installed package by name.
+echo   upgrade  - Upgrade an installed package to the latest version.
+echo   search   - Open the package database in a browser.
+echo   list     - List all installed packages.
+echo   help     - Display this help message.
 exit
 
 :parse
 
 rem Check the first argument
 if "%1"=="install" (
+    if "%2"=="" (
+        echo Package name is required.
+        exit /b 1
+    )
     goto install
 ) else if "%1"=="remove" (
+    if "%2"=="" (
+        echo Package name is required.
+        exit /b 1
+    )
     goto remove
 ) else if "%1"=="upgrade" (
+    if "%2"=="" (
+        echo Package name is required.
+        exit /b 1
+    )
     goto upgrade
+) else if "%1"=="help" (
+    goto help
 ) else if "%1"=="search" (
     goto search
 ) else if "%1"=="list" (
@@ -83,7 +103,7 @@ if exist "C:\Users\%USERNAME%\kali\usr\bin\%2" (
 rem Save package contents to C:\Users\%USERNAME%\kali\tmp\contents.txt
 
 echo Fetching package contents...
-curl -# https://raw.githubusercontent.com/Kali-in-Batch/pkg/refs/heads/main/packages/%2/%2.sh >"C:\Users\%USERNAME%\kali\tmp\contents.txt"
+curl -# https://raw.githubusercontent.com/Kali-in-Batch/pkg/refs/heads/main/packages/%2/rootfs/usr/bin/%2 >"C:\Users\%USERNAME%\kali\tmp\contents.txt"
 
 rem Check if the contents are "404: Not Found"
 
@@ -103,15 +123,19 @@ if %errorlevel%==1 (
 
 rem Save package contents to C:\Users\%USERNAME%\kali\usr\bin\%2
 echo Installing package %2...
-curl -# https://raw.githubusercontent.com/Kali-in-Batch/pkg/refs/heads/main/packages/%2/%2.sh >"C:\Users\%USERNAME%\kali\usr\bin\%2"
+curl -# https://raw.githubusercontent.com/Kali-in-Batch/pkg/refs/heads/main/packages/%2/rootfs/usr/bin/%2 >"C:\Users\%USERNAME%\kali\usr\bin\%2"
+curl -# https://raw.githubusercontent.com/Kali-in-Batch/pkg/refs/heads/main/packages/%2/rootfs/usr/lib/%2.lib >"C:\Users\%USERNAME%\kali\usr\lib\%2.lib"
+curl -# https://raw.githubusercontent.com/Kali-in-Batch/pkg/refs/heads/main/packages/%2/rootfs/usr/libexec/%2.libexec >"C:\Users\%USERNAME%\kali\usr\libexec\%2.libexec
+curl -# https://raw.githubusercontent.com/Kali-in-Batch/pkg/refs/heads/main/packages/%2/rootfs/usr/share/%2.share >"C:\Users\%USERNAME%\kali\usr\share\%2.share"
+curl -# https://raw.githubusercontent.com/Kali-in-Batch/pkg/refs/heads/main/packages/%2/rootfs/usr/local/%2.local >"C:\Users\%USERNAME%\kali\usr\local\%2.local"
 echo. >"C:\Users\%USERNAME%\kali\usr\bin\%2.sh"
-curl -# https://raw.githubusercontent.com/Kali-in-Batch/pkg/refs/heads/main/packages/%2/preinstall.sh >"C:\Users\%USERNAME%\kali\tmp\%2_preinstall"
+curl -# https://raw.githubusercontent.com/Kali-in-Batch/pkg/refs/heads/main/packages/%2/PREINSTALL >"C:\Users\%USERNAME%\kali\tmp\%2_preinstall"
 set /p preinstaller_contents=<C:\Users\%USERNAME%\kali\tmp\%2_preinstall
 if "!contents!"=="404: Not Found" (
     echo Package %2 has no preinstall script
 ) else (
     echo Running preinstall script for package %2...
-    "C:\Users\%USERNAME%\kali\usr\bin\bash.exe" -c "/tmp/%2_preinstall"
+    "C:\Users\%USERNAME%\kali\usr\bin\busybox.exe" bash -c "C:/Users/%USERNAME%/kali/tmp/%2_preinstall"
 )
 echo Package %2 installed successfully.
 del "C:\Users\%USERNAME%\kali\tmp\contents.txt"
@@ -121,13 +145,17 @@ exit
 
 rem Check if the package is installed
 if not exist "C:\Users\%USERNAME%\kali\usr\bin\%2" (
-    echo Package %2 is not installed.
+    echo Package %2 is not installed. Install it by running: pkg install %2
     exit /b
 )
 
 rem Remove the package
 del "C:\Users\%USERNAME%\kali\usr\bin\%2"
 del "C:\Users\%USERNAME%\kali\usr\bin\%2.sh"
+del "C:\Users\%USERNAME%\kali\usr\lib\%2.lib"
+del "C:\Users\%USERNAME%\kali\usr\libexec\%2.libexec"
+del "C:\Users\%USERNAME%\kali\usr\share\%2.share"
+del "C:\Users\%USERNAME%\kali\usr\local\%2.local"
 echo Package %2 removed successfully.
 exit
 
@@ -139,18 +167,19 @@ if not exist "C:\Users\%USERNAME%\kali\usr\bin\%2" (
     exit /b
 )
 
-rem Compare the package contents
-curl -# https://raw.githubusercontent.com/Kali-in-Batch/pkg/refs/heads/main/packages/%2/%2.sh >"C:\Users\%USERNAME%\kali\tmp\contents.txt"
-set /p contents=<C:\Users\%USERNAME%\kali\tmp\contents.txt
-set /p oldcontents=<C:\Users\%USERNAME%\kali\usr\bin\%2
+rem Save package contents to C:\Users\%USERNAME%\kali\tmp\contents.txt
+
+echo Fetching package contents for upgrade...
+curl -# https://raw.githubusercontent.com/Kali-in-Batch/pkg/refs/heads/main/packages/%2/rootfs/usr/bin/%2 >"C:\Users\%USERNAME%\kali\tmp\contents.txt"
 
 rem Check if the contents are "404: Not Found"
+
+set /p contents=<C:\Users\%USERNAME%\kali\tmp\contents.txt
 
 if "!contents!"=="404: Not Found" (
     echo Package %2 is not available anymore. Sorry!
     exit /b
 )
-
 
 if %errorlevel%==1 (
     echo Package %2 is not available anymore. Sorry!
@@ -160,16 +189,20 @@ if %errorlevel%==1 (
 )
 
 rem Upgrade the package
-
 echo Upgrading package %2...
-curl -# https://raw.githubusercontent.com/Kali-in-Batch/pkg/refs/heads/main/packages/%2/%2.sh >"C:\Users\%USERNAME%\kali\usr\bin\%2"
-curl -# https://raw.githubusercontent.com/Kali-in-Batch/pkg/refs/heads/main/packages/%2/preinstall.sh >"C:\Users\%USERNAME%\kali\tmp\%2_preinstall"
+curl -# https://raw.githubusercontent.com/Kali-in-Batch/pkg/refs/heads/main/packages/%2/rootfs/usr/bin/%2 >"C:\Users\%USERNAME%\kali\usr\bin\%2"
+curl -# https://raw.githubusercontent.com/Kali-in-Batch/pkg/refs/heads/main/packages/%2/rootfs/usr/lib/%2.lib >"C:\Users\%USERNAME%\kali\usr\lib\%2.lib"
+curl -# https://raw.githubusercontent.com/Kali-in-Batch/pkg/refs/heads/main/packages/%2/rootfs/usr/libexec/%2.libexec >"C:\Users\%USERNAME%\kali\usr\libexec\%2.libexec"
+curl -# https://raw.githubusercontent.com/Kali-in-Batch/pkg/refs/heads/main/packages/%2/rootfs/usr/share/%2.share >"C:\Users\%USERNAME%\kali\usr\share\%2.share"
+curl -# https://raw.githubusercontent.com/Kali-in-Batch/pkg/refs/heads/main/packages/%2/rootfs/usr/local/%2.local >"C:\Users\%USERNAME%\kali\usr\local\%2.local"
+echo. >"C:\Users\%USERNAME%\kali\usr\bin\%2.sh"
+curl -# https://raw.githubusercontent.com/Kali-in-Batch/pkg/refs/heads/main/packages/%2/PREINSTALL >"C:\Users\%USERNAME%\kali\tmp\%2_preinstall"
 set /p preinstaller_contents=<C:\Users\%USERNAME%\kali\tmp\%2_preinstall
 if "!contents!"=="404: Not Found" (
     echo Package %2 has no preinstall script
 ) else (
     echo Running preinstall script for package %2...
-    "C:\Users\%USERNAME%\kali\usr\bin\bash.exe" -c "/tmp/%2_preinstall"
+    "C:\Users\%USERNAME%\kali\usr\bin\busybox.exe" bash -c "C:/Users/%USERNAME%/kali/tmp/%2_preinstall"
 )
 echo Package %2 upgraded successfully.
 
--- src/kali_in_batch.bat
+++ src/kali_in_batch.bat
@@ -20,6 +20,16 @@ rem
 rem You should have received a copy of the GNU General Public License
 rem along with this program; if not, write to the Free Software
 rem Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
+rem
+rem -- Other Scripts --
+rem
+rem * %~dp0bin\clear.bat - Clears the console
+rem * %~dp0bin\pkg.bat - Manages packages
+rem * %~dp0bin\touch.bat - Creates a new file
+rem * %~dp0bin\uname.bat - Displays system information
+rem * %~dp0bin\which.bat - Displays location of a file or directory in the PATH
+rem * %~dp0bin\whoami.bat - Displays the current user
+rem * %~dp0bin\msfconsole.bat - Uses the Windows Subsystem for Linux to launch the Metasploit Framework
 
 
 rem Color Definitions
@@ -90,14 +100,6 @@ if not exist "%APPDATA%\kali_in_batch" (
         start https://github.com/microsoft/winget-cli
         exit /b
     )
-
-    if not exist "%~dp0bin\busybox.exe" (
-        echo Bash not found. Did you forget to run setup.bat?
-        pause >nul
-        exit /b
-    )
-
-    set "busybox_path=%~dp0bin\busybox.exe"
     
     cls
     echo !COLOR_INFO!Kali in Batch Installer!COLOR_RESET!
@@ -146,15 +148,17 @@ if not exist "%APPDATA%\kali_in_batch" (
         echo Creating directories...
         mkdir "!kaliroot!\home"
         mkdir "!kaliroot!\home\!username!"
-        mkdir "!kaliroot!\bin"
         mkdir "!kaliroot!\tmp"
         mkdir "!kaliroot!\usr"
         mkdir "!kaliroot!\etc"
         mkdir "!kaliroot!\usr\bin"
+        rem Make a directory junction so /bin is /usr/bin
+        mklink /j "!kaliroot!\bin" "!kaliroot!\usr\bin" >nul
         mkdir "!kaliroot!\usr\lib"
         mkdir "!kaliroot!\usr\share"
         mkdir "!kaliroot!\usr\local"
         mkdir "!kaliroot!\usr\libexec"
+        mklink /j "!kaliroot!\lib" "!kaliroot!\usr\lib" >nul
         mkdir "!kaliroot!\var"
         mkdir "!kaliroot!\root"
         xcopy "%~dp0bin\*" "!kaliroot!\usr\bin\" /s /y >nul
@@ -162,8 +166,6 @@ if not exist "%APPDATA%\kali_in_batch" (
         xcopy "%~dp0lib\*" "!kaliroot!\usr\lib\" /s /y >nul
         xcopy "%~dp0share\*" "!kaliroot!\usr\share\" /s /y >nul
         xcopy "%~dp0libexec\*" "!kaliroot!\usr\libexec\" /s /y >nul
-        copy /y "%~dp0kibenv" "!kaliroot!\etc\.kibenv" >nul
-        copy /y "%~dp0os-release" "!kaliroot!\etc\os-release" >nul
         echo # Add commands to run on startup here>"!kaliroot!\home\!username!\.bashrc"
         echo # Add commands to run on startup here>"!kaliroot!\root\.bashrc"
         echo Checking dependencies...
@@ -178,6 +180,9 @@ if not exist "%APPDATA%\kali_in_batch" (
         echo Installing Neovim from winget...
         winget install --id Neovim.Neovim -e --source winget
     )
+    echo Downloading busybox...
+    curl -L -o "!kaliroot!\usr\bin\busybox.exe" "https://frippery.org/files/busybox/busybox64u.exe" -#
+    set "busybox_path=!kaliroot!\usr\bin\busybox.exe"
     mkdir "%APPDATA%\kali_in_batch" >nul 2>&1
     @echo on
     echo !kaliroot!>"%APPDATA%\kali_in_batch\kaliroot.txt"
@@ -215,7 +220,7 @@ for /f "delims=" %%i in ('powershell -command "[System.Environment]::OSVersion.V
 
 cls
 
-echo Welcome to Kali in Batch 8.0 ^(%PROCESSOR_ARCHITECTURE%^)
+echo Welcome to Kali in Batch 9.0 ^(%PROCESSOR_ARCHITECTURE%^)
 echo Booting system...
 echo ------------------------------------------------
 ::                                                                 |
@@ -250,6 +255,7 @@ if not exist "!kaliroot!\home\!username!" set rescue_required=1
 if not exist "!kaliroot!\usr\libexec" set rescue_required=1
 if not exist "!kaliroot!\var" set rescue_required=1
 if not exist "!kaliroot!\root" set rescue_required=1
+if not exist "!kaliroot!\lib" set rescue_required=1
 
 if %rescue_required%==1 (
     if not exist "!kaliroot!\etc" mkdir "!kaliroot!\etc"
@@ -266,12 +272,13 @@ if %rescue_required%==1 (
     if not exist "!kaliroot!\usr\share" mkdir "!kaliroot!\usr\share"
     if not exist "!kaliroot!\usr\lib" mkdir "!kaliroot!\usr\lib"
     if not exist "!kaliroot!\usr\local" mkdir "!kaliroot!\usr\local"
-    if not exist "!kaliroot!\usr\libexec" mkdir "!kaliroot!\usr\libexec
-    if not exist "!kaliroot!\bin" mkdir "!kaliroot!\bin"
+    if not exist "!kaliroot!\usr\libexec" mkdir "!kaliroot!\usr\libexec"
+    if not exist "!kaliroot!\bin" mklink /j "!kaliroot!\bin" "!kaliroot!\usr\bin" >nul
     if not exist "!kaliroot!\home" mkdir "!kaliroot!\home"
     if not exist "!kaliroot!\home\!username!" mkdir "!kaliroot!\home\!username!"
     if not exist "!kaliroot!\var" mkdir "!kaliroot!\var"
     if not exist "!kaliroot!\root" mkdir "!kaliroot!\root"
+    if not exist "!kaliroot!\lib" mklink /j "!kaliroot!\lib" "!kaliroot!\usr\lib" >nul
 )
 
 <nul set /p "=[ !COLOR_SUCCESS!OK!COLOR_RESET! ]"
@@ -287,20 +294,106 @@ set "ROOT=0"
 echo.
 
 ::                                                                 |
-<nul set /p "=Updating core files...                               "
+<nul set /p "=Copying core files from local repository...          "
 
 xcopy "%~dp0bin\*" "!kaliroot!\usr\bin\" /s /y >nul 2>&1
 xcopy "%~dp0etc\*" "!kaliroot!\etc\" /s /y >nul 2>&1
 xcopy "%~dp0lib\*" "!kaliroot!\usr\lib\" /s /y >nul 2>&1
 xcopy "%~dp0share\*" "!kaliroot!\usr\share\" /s /y >nul 2>&1
 xcopy "%~dp0libexec\*" "!kaliroot!\usr\libexec\" /s /y >nul 2>&1
-copy /y "%~dp0kibenv" "!kaliroot!\etc\.kibenv" >nul 2>&1
-copy /y "%~dp0os-release" "!kaliroot!\etc\os-release" >nul 2>&1
+(
+    echo #!/bin/bash
+    echo.
+    echo ########################
+    echo #                      #
+    echo #       WARNING        #
+    echo #                      #
+    echo #  This script is      #
+    echo #  NOT intended for    #
+    echo #  modification in the #
+    echo #  Kali in Batch       #
+    echo #  environment. It is  #
+    echo #  overwritten when    #
+    echo #  you boot Kali in    #
+    echo #  Batch.              #
+    echo #                      #
+    echo #  To add your own     #
+    echo #  configurations,     #
+    echo #  modify ~/.bashrc    #
+    echo #  instead.            #
+    echo #                      #
+    echo ########################
+    echo.
+    echo ## Kali Linux shell prompt ##
+    echo.
+    echo # Check if ROOT is 1
+    echo.
+    echo if [ "$ROOT" == "1" ]; then
+    echo     PS1=$'\[\e[34m\]┌──^(\[\e[31m\]root㉿\h\[\e[34m\]^)-[\[\e[0m\]\w\[\e[34m\]]\n\[\e[34m\]└─\[\e[31m\]# \[\e[0m\]'
+    echo else
+    echo     PS1=$'\[\e[32m\]┌──^(\[\e[34m\]\u㉿\h\[\e[32m\]^)-[\[\e[0m\]\w\[\e[32m\]]\n\[\e[32m\]└─\[\e[34m\]$ \[\e[0m\]'
+    echo fi
+    echo.
+    echo ## Changes to PATH ##
+    echo.
+    echo export PATH="C:/Users/$USERNAME/kali/usr/bin:$PATH"
+    echo.
+    echo ## Aliases ##
+    echo.
+    echo alias clear="clear.bat"
+    echo alias pkg="pkg.bat"
+    echo alias touch="touch.bat"
+    echo alias uname="uname.bat"
+    echo alias which="which.bat"
+    echo alias whoami="whoami.bat"
+    echo alias msfconsole="msfconsole.bat"
+    echo.
+    echo alias netcat="nc"
+    echo.
+    echo ## Functions ##
+    echo.
+    echo sudo^(^) {
+    echo     export ROOT="1"
+    echo     export USER="root"
+    echo     "$@" # Run the arguments
+    echo     export ROOT="0"
+    echo     export USER="$USERNAME"
+    echo }
+    echo.
+    echo su^(^) {
+    echo     export ROOT="1"
+    echo     export USER="root"
+    echo     PS1=$'\[\e[34m\]┌──^(\[\e[31m\]root㉿\h\[\e[34m\]^)-[\[\e[0m\]\w\[\e[34m\]]\n\[\e[34m\]└─\[\e[31m\]# \[\e[0m\]'
+    echo }
+    echo.
+    echo unsu^(^) {
+    echo     export ROOT="0"
+    echo     export USER="$USERNAME"
+    echo     PS1=$'\[\e[32m\]┌──^(\[\e[34m\]\u㉿\h\[\e[32m\]^)-[\[\e[0m\]\w\[\e[32m\]]\n\[\e[32m\]└─\[\e[34m\]$ \[\e[0m\]'
+    echo }
+    echo.
+    echo ## Load ~/.bashrc ##
+    echo.
+    echo source ~/.bashrc
+) > "!kaliroot!\etc\.kibenv"
+
+(
+    echo NAME="Kali in Batch"
+    echo VERSION="9.0"
+    echo ID=kalibatch
+    echo ID_LIKE=linux
+    echo VERSION_ID="9.0"
+    echo PRETTY_NAME="Kali in Batch 9.0"
+    echo ANSI_COLOR="0;36"
+    echo HOME_URL="https://kali-in-batch.github.io"
+    echo SUPPORT_URL="https://github.com/Kali-in-Batch/kali-in-batch/discussions"
+    echo BUG_REPORT_URL="https://github.com/Kali-in-Batch/kali-in-batch/issues"
+) > "!kaliroot!\etc\os-release"
 
 if !errorlevel! neq 0 (
     <nul set /p "=[ !COLOR_ERROR!FAILED!COLOR_RESET! ]"
-    pause >nul
-    exit /b 1
+    echo.
+    echo Note: It is very likely that the script did not fail to copy files.
 ) else (
     <nul set /p "=[ !COLOR_SUCCESS!OK!COLOR_RESET! ]"
     echo.
@@ -311,7 +404,7 @@ if exist "%APPDATA%\kali_in_batch\VERSION.txt" (
     del "%APPDATA%\kali_in_batch\VERSION.txt"
 )
 rem Create VERSION.txt
-echo 8.0>"%APPDATA%\kali_in_batch\VERSION.txt"
+echo 9.0>"%APPDATA%\kali_in_batch\VERSION.txt"
 
 ::                                                                 |
 <nul set /p "=Starting Nmap service...                             "
@@ -340,19 +433,15 @@ if !errorlevel! neq 0 (
 ::                                                                 |
 <nul set /p "=Starting Bash service...                             "
 
-if not exist "!kaliroot!\usr\bin\busybox.exe" (
-    <nul set /p "=[ !COLOR_ERROR!FAILED!COLOR_RESET! ]"
-    pause >nul
-    exit /b 1
-) else (
-    <nul set /p "=[ !COLOR_SUCCESS!OK!COLOR_RESET! ]"
-    echo.
-)
+curl -L -o "!kaliroot!\usr\bin\busybox.exe" "https://frippery.org/files/busybox/busybox64u.exe" -s
+
+<nul set /p "=[ !COLOR_SUCCESS!OK!COLOR_RESET! ]"
+echo.
 
 set "busybox_path=!kaliroot!\usr\bin\busybox.exe"
 
 ::                                                                 |
-<nul set /p "=Checking for updates...                              "
+<nul set /p "=Checking for remote updates...                       "
 
 curl -s https://raw.githubusercontent.com/Kali-in-Batch/kali-in-batch/refs/heads/master/VERSION.txt >"!kaliroot!\tmp\VERSION.txt"
 rem Check if the version is the same
@@ -386,7 +475,7 @@ if "%~1"=="automated" (
 :login
 
 cls
-echo Kali in Batch 8.0
+echo Kali in Batch 9.0
 echo Kernel !kernelversion! on an %PROCESSOR_ARCHITECTURE%
 echo.
 echo Users on this system: !username!, root
@@ -395,6 +484,7 @@ set /p loginkibusername=%COMPUTERNAME% login:
 if "!loginkibusername!"=="!username!" (
     rem Correct
     set "USER=!username!"
+    set "ROOT=0"
     echo !COLOR_SUCCESS!User found!COLOR_RESET!
     echo Connecting to Bash service...
     timeout /t 1 /nobreak >nul 2>&1
@@ -446,8 +536,21 @@ if "!ROOT!"=="0" (
 ) else (
     set "HOME=!kaliroot!\root"
 )
-copy /y "%~dp0kibenv" "!HOME!\.profile" >nul 2>&1
+
 if not exist "!HOME!\.bashrc" (
-    echo. >"!HOME!\.bashrc"
+    echo # Add commands to run on startup here>"!HOME!\.bashrc"
 )
+
+set "ENV=C:/Users/!username!/kali/etc/.kibenv"
+
+if not exist "!HOME!\.hushlogin" (
+    echo For a guide on how to use Kali in Batch, run 'ls -l /usr/share/guide' and
+    echo open the text file that you think will help you.
+    echo.
+    echo Example:
+    echo $ less /usr/share/guide/hacking.txt # Less is used here because you can scroll
+    echo.
+    echo To disable this message, create a file called .hushlogin in your home directory.
+)
+
 "!busybox_path!" bash -l
