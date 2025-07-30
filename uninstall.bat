885031752477b47f5929dc138bd31868cae2ae5e
benja2998 <benja2998@duck.com>
2025-07-30 16:50:49 -0400

    Improved uninstaller, make touch.bat a wrapper for busybox touch

--- uninstall.bat
+++ uninstall.bat
@@ -1,12 +1,11 @@
 @echo off
 
-subst
-set /p "driveletter=Virtual drives listed. Please enter the virtual drive used for Kali in Batch with a colon (e.g. Z:). Leave blank if none >> "
-if "%driveletter%"=="" goto skip_subst
-echo Deleting %driveletter%...
-subst %driveletter% /d
-goto skip_subst
-:skip_subst
+set /p kaliroot=<"%APPDATA%\kali_in_batch\kaliroot.txt"
+
+echo Deleting %kaliroot%\...
+
+subst %kaliroot% /d >nul 2>&1
+
 echo Deleting %APPDATA%\kali_in_batch...
 rmdir /s /q "%APPDATA%\kali_in_batch"
 echo Deleting %USERPROFILE%\kali...
