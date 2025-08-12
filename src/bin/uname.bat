7f56a9a9faad3aec27d8f5a94822c55ba60025b0
benja2998 <benja2998@duck.com>
2025-08-12 19:28:27 -0400

    Versio bump

--- src/bin/uname.bat
+++ src/bin/uname.bat
@@ -102,7 +102,7 @@ goto :eof
 
 :version
 
-echo Uname for Kali in Batch 9.12.4
+echo Uname for Kali in Batch 9.12.5
 echo This is GPL-2.0-only licensed free software. There is NO WARRANTY.
 goto :eof
 
