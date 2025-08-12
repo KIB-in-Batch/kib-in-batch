d921e4bd077a2b4dd2d9a4124f3f37adf9bd6f1d
benja2998 <benja2998@duck.com>
2025-08-12 19:40:40 -0400

    Revert "Update kali_in_batch.bat"
    This reverts commit 4f3e543533233bb4bdf2cf88a702ef281a2810de.

--- src/kali_in_batch.bat
+++ src/kali_in_batch.bat
@@ -766,9 +766,13 @@ echo.
     echo     PS1=$'\[\e[32m\]┌──^(\[\e[34m\]\u㉿\h\[\e[32m\]^)-[\[\e[0m\]\w\[\e[32m\]]\n\[\e[32m\]└─\[\e[34m\]$ \[\e[0m\]'
     echo }
     echo.
+    echo ## Exit if not interactive ##
+    echo.
+    echo [[ $- != *i* ]] ^&^& return
+    echo.
     echo ## Load ~/.bashrc ##
     echo.
-    echo [[ $- = *i* ]] && source ~/.bashrc
+    echo source ~/.bashrc
 ) > "!kaliroot!\etc\.kibenv" 2>>"%APPDATA%\kali_in_batch\errors.log"
 
 (
