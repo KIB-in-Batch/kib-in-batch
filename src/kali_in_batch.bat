8af145081fa6f0c3f23e8ccd78f39fbf79d2917d
benja2998 <benja2998@duck.com>
2025-08-12 19:37:41 -0400

    Update kali_in_batch.bat

--- src/kali_in_batch.bat
+++ src/kali_in_batch.bat
@@ -623,9 +623,11 @@ echo.
     echo     PS1=$'\[\e[32m\]┌──^(\[\e[34m\]\u㉿\h\[\e[32m\]^)-[\[\e[0m\]\w\[\e[32m\]]\n\[\e[32m\]└─\[\e[34m\]$ \[\e[0m\]'
     echo }
     echo.
-    echo ## Load ~/.bashrc ##
-    echo.
-    echo [[ $- = *i* ]] ^&^& source ~/.bashrc
+    echo if [[ $- = *i* ]]; then
+    echo     # Session is interactive
+    echo     # Load ~/.bashrc
+    echo     source ~/.bashrc
+    echo fi
 ) > "!kaliroot!\etc\.kibenv" 2>>"%APPDATA%\kali_in_batch\errors.log"
 
 (
