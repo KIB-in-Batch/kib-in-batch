691b4a047239197b5a4a4ffa9b909b1b0aeb68b7
benja2998 <benja2998@duck.com>
2025-08-20 15:12:58 -0400

    Version bump

--- src/bin/uname.bat
+++ src/bin/uname.bat
@@ -102,7 +102,7 @@ goto :eof
 
 :version
 
-echo Uname for KIB in Batch 10.2.0
+echo Uname for KIB in Batch 10.2.1
 echo This is GPL-2.0-only licensed free software. There is NO WARRANTY.
 goto :eof
 
